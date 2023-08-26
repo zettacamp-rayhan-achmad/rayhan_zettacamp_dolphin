c362e43dbea4343aec6d0a94177b839bfa128774
zettacamp-rayhan-achmad <rayhan.zidane@zettacamp.pro>
2023-08-26 23:47:08 +0700

    task-1 done

---
+++ task-1.js
@@ -0,0 +1,161 @@
+// function display book detail
+function displayBookDetail() {
+   console.log('-------------------------------------');
+   console.log('-**         BOOK DETAILS          **-');
+   console.log('-------------------------------------');
+   console.log('Book Title:', bookDetail.title);
+   console.log('Price: Rp ', parseInt(bookDetail.price));
+   console.log('Author: ', bookDetail.author);
+   console.log('Stock:', amountStock);
+   console.log('-------------------------------------');
+   return;
+}
+// function display book purchasing
+function displayBookPurchased(
+   discountAmount,
+   priceAfterDiscount,
+   taxAmount,
+   priceAfterTax,
+   priceAfterDiscount,
+   purchasedBook,
+   totalPrice,
+   availableStock,
+   discountAndTax
+) {
+   console.log('            Book Purchased           ');
+   console.log('-------------------------------------');
+   console.log('discount:', discount, '%');
+   console.log('Amount of discount: Rp', discountAmount);
+   console.log('Price after discount: Rp ', priceAfterDiscount);
+   console.log('Tax:', tax, '%');
+   console.log('Amount of Tax: Rp', taxAmount);
+   console.log('Price after Tax: Rp', priceAfterTax);
+   console.log('Amount of Book Purchased:', purchasedBook);
+   console.log('Remaining Stock:', availableStock);
+   console.log('Is tax greater than discount:', discountAndTax);
+   console.log('------------------------------------');
+}
+// function credit term
+function determineCreditTerms(totalPrice, terms) {
+   const duePerTerm = totalPrice / terms;
+   const currentDate = new Date();
+   const nextMonth = new Date(currentDate);
+   nextMonth.setMonth(currentDate.getMonth() + 1);
+
+   let creditTerms = [];
+   for (let i = 0; i < terms; i++) {
+      const dueDate = new Date(nextMonth);
+      dueDate.setMonth(nextMonth.getMonth() + i);
+
+      creditTerms.push({
+         term: i + 1,
+         due: dueDate.toISOString().split('T')[0],
+         amountDue: duePerTerm,
+      });
+   }
+
+   //  return creditTerms;
+   console.log('------------------------------------');
+   console.log('**        Credit Simulation       **');
+   for (let i = 0; i < creditTerms.length; i++) {
+      const book = creditTerms[i];
+      console.log('------------------------------------');
+      console.log(`Term: ${book.term}`);
+      console.log(`Due: ${book.due}`);
+      console.log(`Amout Must Pay: ${book.amountDue}`);
+   }
+}
+// function purchasing books
+function purchaseBooks(bookDetail, discount, tax, amountStock, purchasedBook) {
+   let discountAmount = (discount / 100) * parseInt(bookDetail.price);
+   let priceAfterDiscount = parseInt(bookDetail.price - discountAmount);
+   let taxAmount = parseInt((tax / 100) * priceAfterDiscount);
+   let priceAfterTax = priceAfterDiscount + taxAmount;
+   let result = tax > discount ? true : false;
+   let discountAndTax = result === true ? 'Yes' : 'No';
+   const bookPrice = bookDetail.price;
+   let totalPrice = 0;
+   let availableStock = amountStock;
+
+   for (let i = 1; i <= purchasedBook; i++) {
+      if (availableStock === 0) {
+         displayBookDetail();
+         displayBookPurchased(
+            discountAmount,
+            priceAfterDiscount,
+            taxAmount,
+            priceAfterTax,
+            priceAfterDiscount,
+            purchasedBook,
+            totalPrice,
+            availableStock,
+            discountAndTax
+         );
+         console.log(`Out of stock, only purchasing ${i - 1} books.`);
+         console.log(`cannot buy book any more`);
+         break;
+      }
+
+      totalPrice += bookPrice; // Adding book price
+      availableStock--;
+
+      if (i === purchasedBook) {
+         if (availableStock > 0) {
+            displayBookDetail();
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased ${purchasedBook} books. ${availableStock} books can still be purchased.`
+            );
+         } else {
+            displayBookDetail();
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased all ${purchasedBook} books. you cannot buy book any more`
+            );
+         }
+      }
+   }
+
+   totalPrice -= totalPrice * (discount / 100);
+   totalPrice += totalPrice * (tax / 100);
+
+   console.log(`Total price: Rp ${totalPrice}`);
+
+   determineCreditTerms(totalPrice, terms);
+}
+
+function book(title, price, author) {
+   this.title = title;
+   this.price = price;
+   this.author = author;
+   return;
+}
+
+let bookDetail = new book('Naruto Shippuden', 14000, 'Masashi Kisimoto');
+
+const discount = 10; // 10% discount
+const tax = 5; // 5% tax
+const amountStock = 4; // Initial stock of books
+const purchasedBook = 4; // Number of books to be purchased
+const terms = 2;
+purchaseBooks(bookDetail, discount, tax, amountStock, purchasedBook);
