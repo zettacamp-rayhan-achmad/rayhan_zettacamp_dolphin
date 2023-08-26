a527e4e8cd177eb4e713360423cf07a37707aae3
zettacamp-rayhan-achmad <rayhan.zidane@zettacamp.pro>
2023-08-26 15:04:56 +0700

    rename logic test

--- task-1.js
+++ task-1.js
@@ -1,126 +1,129 @@
-try {
-   // Function
-   function bookPurchasing(
-      bookDetail,
-      discount,
-      tax,
-      amountStock,
-      purchasedBook
-   ) {
-      let discountAmount = (discount / 100) * parseInt(bookDetail.price);
-      let priceAfterDiscount = parseInt(bookDetail.price - discountAmount);
-      let taxAmount = parseInt((tax / 100) * priceAfterDiscount);
-      let priceAfterTax = priceAfterDiscount + taxAmount;
-      let totalPrice = priceAfterTax * purchasedBook;
-      let result = tax > discount ? true : false;
-      let discountAndTax = result === true ? 'Yes' : 'No';
-      if (purchasedBook > amountStock) {
-         displayBookDetail();
-         console.log('amount purchased :', purchasedBook);
-         console.log('!book out of stock, to many purchased');
-         return;
-      } else {
-         remainingMoreStock = amountStock - purchasedBook;
-         for (let i = 0; i < purchasedBook; i++) {
-            if (remainingMoreStock === 0) {
-               console.log('-------------------------------------');
-               console.log('** book purchase successful,');
-               console.log('but the book is out of stock.');
-               console.log('no books to buy');
-               break;
-            }
+// function display book detail
+function displayBookDetail() {
+   console.log('-------------------------------------');
+   console.log('-**         BOOK DETAILS          **-');
+   console.log('-------------------------------------');
+   console.log('Book Title:', bookDetail.title);
+   console.log('Price: Rp ', parseInt(bookDetail.price));
+   console.log('Author: ', bookDetail.author);
+   console.log('Stock:', amountStock);
+   console.log('-------------------------------------');
+   return;
+}
+// function display book purchasing
+function displayBookPurchased(
+   discountAmount,
+   priceAfterDiscount,
+   taxAmount,
+   priceAfterTax,
+   priceAfterDiscount,
+   purchasedBook,
+   totalPrice,
+   availableStock,
+   discountAndTax
+) {
+   console.log('            Book Purchased           ');
+   console.log('-------------------------------------');
+   console.log('discount:', discount, '%');
+   console.log('Amount of discount: Rp', discountAmount);
+   console.log('Price after discount: Rp ', priceAfterDiscount);
+   console.log('Tax:', tax, '%');
+   console.log('Amount of Tax: Rp', taxAmount);
+   console.log('Price after Tax: Rp', priceAfterTax);
+   console.log('Amount of Book Purchased:', purchasedBook);
+   console.log('Remaining Stock:', availableStock);
+   console.log('Is tax greater than discount:', discountAndTax);
+   console.log('------------------------------------');
+}
+// function purchasing books
+function purchaseBooks(bookDetail, discount, tax, amountStock, purchasedBook) {
+   let discountAmount = (discount / 100) * parseInt(bookDetail.price);
+   let priceAfterDiscount = parseInt(bookDetail.price - discountAmount);
+   let taxAmount = parseInt((tax / 100) * priceAfterDiscount);
+   let priceAfterTax = priceAfterDiscount + taxAmount;
+   let result = tax > discount ? true : false;
+   let discountAndTax = result === true ? 'Yes' : 'No';
+   const bookPrice = bookDetail.price;
+   let totalPrice = 0;
+   let availableStock = amountStock;
 
-            // if (remainingMoreStock > 0) {
-            //    console.log('-------------------------------------');
-            //    console.log(
-            //       `** Book ${bookDetail.title} can still be purchased ${remainingMoreStock} more times.`
-            //    );
-            //    break;
-            // }
-            else {
-               console.log('-------------------------------------');
-               console.log(
-                  `** Book ${bookDetail.title} can still be purchased ${remainingMoreStock} more times.`
-               );
-               console.log(
-                  `** Book ${bookDetail.title} can still be purchased ${
-                     i - 1
-                  } more times.`
-               );
-               // break;
-            }
-         }
+   for (let i = 1; i <= purchasedBook; i++) {
+      if (availableStock === 0) {
+         displayBookDetail();
+         displayBookPurchased(
+            discountAmount,
+            priceAfterDiscount,
+            taxAmount,
+            priceAfterTax,
+            priceAfterDiscount,
+            purchasedBook,
+            totalPrice,
+            availableStock,
+            discountAndTax
+         );
+         console.log(`Out of stock, only purchasing ${i - 1} books.`);
+         console.log(`cannot buy book any more`);
+         break;
       }
 
-      let remainingStock =
-         purchasedBook > amountStock ? amountStock : remainingMoreStock;
-      displayBookDetail();
-      displayBookPurchased(
-         discountAmount,
-         priceAfterDiscount,
-         taxAmount,
-         priceAfterTax,
-         priceAfterDiscount,
-         purchasedBook,
-         totalPrice,
-         remainingStock,
-         discountAndTax
-      );
+      totalPrice += bookPrice; // Adding book price
+      availableStock--;
 
-      return;
+      if (i === purchasedBook) {
+         if (availableStock > 0) {
+            displayBookDetail();
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased ${purchasedBook} books. ${availableStock} books can still be purchased.`
+            );
+         } else {
+            displayBookDetail();
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased all ${purchasedBook} books. you cannot buy book any more`
+            );
+         }
+      }
    }
 
-   function displayBookDetail() {
-      console.log('-------------------------------------');
-      console.log('-**         BOOK DETAILS          **-');
-      console.log('-------------------------------------');
-      console.log('Book Title:', bookDetail.title);
-      console.log('Price: Rp ', parseInt(bookDetail.price));
-      console.log('Author: ', bookDetail.author);
-      console.log('Stock:', amountStock);
-      console.log('-------------------------------------');
-      return;
-   }
-   function displayBookPurchased(
-      discountAmount,
-      priceAfterDiscount,
-      taxAmount,
-      priceAfterTax,
-      priceAfterDiscount,
-      purchasedBook,
-      totalPrice,
-      remainingStock,
-      discountAndTax
-   ) {
-      console.log('            Book Purchased           ');
-      console.log('-------------------------------------');
-      console.log('discount:', discount, '%');
-      console.log('Amount of discount: Rp', discountAmount);
-      console.log('Price after discount: Rp ', priceAfterDiscount);
-      console.log('Tax:', tax, '%');
-      console.log('Amount of Tax: Rp', taxAmount);
-      console.log('Price after Tax: Rp', priceAfterTax);
-      console.log('Amount of Book Purchased:', purchasedBook);
-      console.log('Total Price: Rp', totalPrice);
-      console.log('Remaining Stock:', remainingStock);
-      console.log('Is tax greater than discount:', discountAndTax);
-      console.log('------------------------------------');
-   }
+   totalPrice -= totalPrice * (discount / 100);
+   totalPrice += totalPrice * (tax / 100);
 
-   // function constractor
-   function book(title, price, author) {
-      this.title = title;
-      this.price = price;
-      this.author = author;
-      return;
-   }
-
-   let bookDetail = new book('Naruto Shippuden', 120000, 'Masashi Kisimoto');
-   let discount = 10;
-   let tax = 5;
-   let purchasedBook = 2;
-   let amountStock = 8;
-   bookPurchasing(bookDetail, discount, tax, amountStock, purchasedBook);
-} catch (err) {
-   console.log('ERROR!', err.message);
+   console.log(`Total price: Rp ${totalPrice}`);
 }
+
+function book(title, price, author) {
+   this.title = title;
+   this.price = price;
+   this.author = author;
+   return;
+}
+
+let bookDetail = new book('Naruto Shippuden', 14000, 'Masashi Kisimoto');
+
+const discount = 10; // 10% discount
+const tax = 5; // 5% tax
+const amountStock = 4; // Initial stock of books
+const purchasedBook = 4; // Number of books to be purchased
+
+purchaseBooks(bookDetail, discount, tax, amountStock, purchasedBook);
--- uniqueCharacter_Rayhan.js
+++ uniqueCharacter_Rayhan.js
@@ -5,24 +5,19 @@
 //console.log(hasUniqueCharacters("hello")); // Output: false
 
 function hasUniqueCharacters(str) {
-   // Create an object to keep track of characters we've seen
    const seenCharacters = {};
 
-   // Loop through each character in the string
    for (let char of str) {
-      // If the character is already in the object, return false
       if (seenCharacters[char]) {
          console.log(seenCharacters);
          return false;
       }
-      // Mark the character as seen
       console.log(seenCharacters);
       seenCharacters[char] = true;
    }
 
-   // If we haven't returned false, all characters are unique
    return true;
 }
 
-console.log(hasUniqueCharacters('abcdef')); // Output: true
-console.log(hasUniqueCharacters('hallo')); // Output: false
+console.log(hasUniqueCharacters('abcdefg')); // Output: true
+console.log(hasUniqueCharacters('kakiku')); // Output: false
