3a85e62ed950c783f7b5755d98b690d68a360a51
zettacamp-rayhan-achmad <rayhan.zidane@zettacamp.pro>
2023-08-27 13:43:22 +0700

    first commit

---
+++ majorityElement_Rayhan.js
@@ -0,0 +1,18 @@
+/**
+ * write a function that returns the majority element.
+ * The majority element is the element that appears more than other element.
+ * READ EXAMPLE BELOW!
+
+console.log(majorityElement([3, 2, 3])); // Output: 3 
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2 
+
+ * You may assume that the majority element always exists in the array.
+
+ * Returns the majority element from the input array of integers.
+
+ * @param {number[]} nums - The input array of integers.
+ * @return {number} Returns the majority element.
+ */
+function majorityElement(nums) {
+   // Your logic here
+}
---
+++ task-1.js
@@ -0,0 +1,177 @@
+// function display book detail
+function displayBookDetail(bookDetail) {
+   console.log('-------------------------------------');
+   console.log('**         BOOK PURCHASED          **');
+   console.log('-------------------------------------');
+   console.log('Book Title:', bookDetail.title);
+   console.log('Price: Rp ', parseInt(bookDetail.price));
+   console.log('Author: ', bookDetail.author);
+   console.log('Previously Stock:', amountStock);
+   return;
+}
+// function display book purchasing
+function displayBookPurchased(
+   discountAmount,
+   priceAfterDiscount,
+   taxAmount,
+   priceAfterTax,
+   priceAfterDiscount,
+   purchasedBook,
+   totalPrice,
+   availableStock,
+   discountAndTax
+) {
+   console.log('discount:', discount, '%');
+   console.log('Amount of discount: Rp', discountAmount);
+   console.log('Price after discount: Rp ', priceAfterDiscount);
+   console.log('Tax:', tax, '%');
+   console.log('Amount of Tax: Rp', taxAmount);
+   console.log('Price after Tax: Rp', priceAfterTax);
+   console.log('Amount of Book Purchased:', purchasedBook);
+   console.log('Remaining Stock:', availableStock);
+   console.log('Is tax greater than discount:', discountAndTax);
+   console.log('------------------------------------');
+}
+// function credit terms
+function determineCreditTerms(totalPrice, terms) {
+   const duePerTerm = totalPrice / terms;
+   const currentDate = new Date();
+   const nextMonth = new Date(currentDate);
+   nextMonth.setMonth(currentDate.getMonth() + 1);
+
+   let creditTerms = [];
+   for (let i = 0; i < terms; i++) {
+      const dueDate = new Date(nextMonth);
+      dueDate.setMonth(nextMonth.getMonth() + i);
+
+      creditTerms.push({
+         term: i + 1,
+         due: dueDate.toISOString().split('T')[0],
+         amountDue: duePerTerm,
+      });
+   }
+
+   //  return creditTerms;
+   console.log('------------------------------------');
+   console.log('**        Credit Simulation       **');
+   for (const book of creditTerms) {
+      console.log('------------------------------------');
+      console.log(`Term: ${book.term}`);
+      console.log(`Due: ${book.due}`);
+      console.log(`Amount Must Pay: Rp ${book.amountDue}`);
+   }
+}
+// function purchasing books
+function purchaseBooks(bookDetail, discount, tax, amountStock, purchasedBook) {
+   let discountAmount = (discount / 100) * parseInt(bookDetail.price);
+   let priceAfterDiscount = parseInt(bookDetail.price - discountAmount);
+   let taxAmount = parseInt((tax / 100) * priceAfterDiscount);
+   let priceAfterTax = priceAfterDiscount + taxAmount;
+   let result = tax > discount ? true : false;
+   let discountAndTax = result === true ? 'Yes' : 'No';
+   const bookPrice = bookDetail.price;
+   let totalPrice = 0;
+   let availableStock = amountStock;
+
+   for (let i = 1; i <= purchasedBook; i++) {
+      if (availableStock === 0) {
+         displayBookDetail(bookDetail);
+         displayBookPurchased(
+            discountAmount,
+            priceAfterDiscount,
+            taxAmount,
+            priceAfterTax,
+            priceAfterDiscount,
+            purchasedBook,
+            totalPrice,
+            availableStock,
+            discountAndTax
+         );
+         console.log(`Out of stock, only purchasing ${i - 1} books.`);
+         console.log(`cannot buy book any more`);
+         break;
+      }
+
+      totalPrice += bookPrice; // Adding book price
+      availableStock--;
+
+      if (i === purchasedBook) {
+         if (availableStock > 0) {
+            displayBookDetail(bookDetail);
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased ${purchasedBook} books. ${availableStock} books can still be purchased.`
+            );
+         } else {
+            displayBookDetail(bookDetail);
+            displayBookPurchased(
+               discountAmount,
+               priceAfterDiscount,
+               taxAmount,
+               priceAfterTax,
+               priceAfterDiscount,
+               purchasedBook,
+               totalPrice,
+               availableStock,
+               discountAndTax
+            );
+            console.log(
+               `Purchased all ${purchasedBook} books. you cannot buy book any more`
+            );
+         }
+      }
+   }
+
+   totalPrice -= totalPrice * (discount / 100);
+   totalPrice += totalPrice * (tax / 100);
+
+   console.log(`Total price: Rp ${totalPrice}`);
+
+   determineCreditTerms(totalPrice, terms);
+}
+
+let bookDetail = [
+   {
+      title: 'The Lord of The Rings',
+      author: 'J.R.R. Tolkien',
+      price: 71000,
+   },
+   {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      price: 82000,
+   },
+   {
+      title: 'The Alchemist',
+      author: 'Paulo Coelho',
+      price: 65000,
+   },
+];
+
+const bookTitle = bookDetail.map((book) => book.title);
+
+// initial
+const discount = 10;
+const tax = 5;
+const amountStock = 5;
+const purchasedBook = 4;
+const terms = 1;
+
+// Run Function and Display Purchase
+console.log('-------------------------------------');
+console.log('**            All Books            **');
+console.log('-------------------------------------');
+for (let book of bookTitle) {
+   console.log(book);
+}
+purchaseBooks(bookDetail[0], discount, tax, amountStock, purchasedBook);
