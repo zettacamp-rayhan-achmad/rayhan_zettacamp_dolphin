9211e0cc0025bf3dbd51f651bc6fc7f5d9f8313b
zettacamp-rayhan-achmad <rayhan.zidane@zettacamp.pro>
2023-09-22 10:32:20 +0700

    match, sort, concat, lookup

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-﻿# rayhan_zettacamp_dolphin
-
+﻿**rayhan_zettacamp_dolphin**
 **Name:** Rayhan Zidane Achmad
-
 **Position:** Back-End
 
+### explain $match
+match adalah salah satu operator dalam operasi aggregasi yang digunakan untuk memfilter dokumen untuk meneruskan hanya dokumen yang cocok dengan kondisi yang ditentukan ke tahap alur berikutnya.
+Misalnya, dalam kasus ini saya ingin mencari buku dengan genre (fantasy) maka akan dipilih buku hanya yang memiliki genre fantasy.
+
+### explain $sort
+sort digunakan untuk mengurutkan dokumen dalam hasil agregasi berdasarkan satu atau lebih kriteria tertentu. Dengan menggunakan $sort, kita dapat mengontrol urutan keluaran dokumen yang diperoleh melalui operasi agregasi. 
+kita dapat menentukan apakah ingin mengurutkan secara ascending 1 (dari yang terkecil ke yang terbesar) atau descending -1 (dari yang terbesar ke yang terkecil).
+
+### explain $concat
+concat adalah operator dalam operasi agregasi yang digunakan untuk menggabungkan beberapa nilai string menjadi satu nilai string tunggal. dalam kasus ini saya menggabungkan genre lama dengan genre tambahan.
+
+### explain $lookup
+lookup adalah salah satu operasi agregasi dalam MongoDB yang digunakan untuk menggabungkan data dari dua koleksi (collections) yang berbeda dalam sebuah database. Ini sangat berguna ketika kita memiliki dua koleksi yang berhubungan dan ingin mengambil data dari kedua koleksi tersebut dalam satu query.
+Misalnya, dalam kasus ini saya memiliki koleksi "bookshelves" dan koleksi "book", saya gunakan $lookup untuk menggabungkannya berdasarkan ID buku.
---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const checkAuth = require('./auth');
+const app = express();
+app.use(express.json());
+const fs = require('fs');
+const {
+   aggregate,
+   unwind,
+   createBookShelves,
+   addBookShelves,
+   getBookShelvesById,
+   getBookShelves,
+   updateBookShelvesById,
+   deleteBookShelvesById,
+   getBasedId,
+   updateBook,
+   updateBookArrayFilter,
+   getDistinctGenre,
+   aggregation,
+   lookup,
+} = require('./controller/book-shelf');
+const {
+   purchaseBookWithTerms,
+   getPurchase,
+   getPurhcaseBookById,
+   createPurchase,
+   updatePurchase,
+   deletePurchaseById,
+   deleteAllPurchase,
+} = require('./controller/purchase-book');
+// app.use(checkAuth);
+const url = 'mongodb://localhost:27017/';
+const database = 'purchaseBooks';
+
+// ***** BOOK PURCHASE *****
+app.post('/purchaseBookWithTerms', purchaseBookWithTerms);
+app.get('/getPurchase', getPurchase);
+app.get('/getPurchaseBookBy/:id', getPurhcaseBookById);
+app.post('/createPurchase', createPurchase);
+app.patch('/updatePurchase/:id', updatePurchase);
+app.delete('/deletePurchase/:id', deletePurchaseById);
+app.delete('/deleteAllPurchase', deleteAllPurchase);
+
+// ****** BOOK SHELVES ******
+app.post('/create-book-shelves', createBookShelves);
+app.post('/add-book-shelves', addBookShelves);
+app.get('/get-bookshelves', getBookShelves);
+app.get('/get-bookshelves-id', getBookShelvesById);
+app.patch('/update-book-shelves/:id', updateBookShelvesById);
+app.delete('/delete-bookshelves-by/:id', deleteBookShelvesById);
+
+// elemMatch
+app.get('/get-based-id', getBasedId);
+app.patch('/update-book-array-filter', updateBookArrayFilter);
+app.get('/get-distinct-genre', getDistinctGenre);
+
+// aggregate
+app.get('/aggregate', aggregate);
+app.get('/unwind', unwind);
+app.get('/aggregation', aggregation);
+app.get('/lookup', lookup);
+
+// ##### database connect #####
+mongoose
+   .connect(`${url}${database}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => {
+      console.log('success! connected to mongoDB');
+   })
+   .catch((error) => {
+      console.error('error connecting to mongoDB', error);
+   });
+
+const port = 3000;
+app.listen(port, () => {
+   console.log(`App running on port ${port}..!`);
+});
