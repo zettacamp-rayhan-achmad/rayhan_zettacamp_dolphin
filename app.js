e21f9140de660d48283d801e2ee5a4a93ab3c22f
zettacamp-rayhan-achmad <rayhan.zidane@zettacamp.pro>
2023-10-10 09:19:16 +0700

    start

--- app.js
+++ app.js
@@ -9,23 +9,15 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
+const { createNewSong, getAllSong, getSongById, updateSong, deleteSongById, aggregateSong, deleteAllSongs } = require('./controller/songs');
 const {
-   createNewSong,
-   getAllSong,
-   getSongById,
-   updateSong,
-   deleteSongById,
-   aggregateSong,
-   deleteAllSongs,
-} = require('./controller/songs');
-const {
-   createNewPlaylist,
-   updatePlaylist,
-   getAllPlayList,
-   deletePlaylist,
-   addSongsToPlaylist,
-   aggregatePlaylist,
-   aggregatePlaylistPercobaan,
+    createNewPlaylist,
+    updatePlaylist,
+    getAllPlayList,
+    deletePlaylist,
+    addSongsToPlaylist,
+    aggregatePlaylist,
+    aggregatePlaylistPercobaan,
 } = require('./controller/playlist');
 
 const url = 'mongodb://127.0.0.1:27017/';
@@ -52,35 +44,35 @@ app.get('/aggregatePlaylist', aggregatePlaylist);
 app.get('/aggregatePlaylistPercobaan', aggregatePlaylistPercobaan);
 
 mongoose
-   .connect(`${url}${database}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-   })
-   .then(() => {
-      console.log('success! connected to mongoDB');
-   })
-   .catch((error) => {
-      console.error('error connecting to mongoDB', error);
-   });
+    .connect(`${url}${database}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('success! connected to mongoDB');
+    })
+    .catch((error) => {
+        console.error('error connecting to mongoDB', error);
+    });
 
 const secretKey = 'rahasia';
 // mongoose.set('debug', true);
 const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   context: ({ req }) => {
-      try {
-         const token = req.headers.authorization || '';
-         const user = jwt.verify(token.replace('Bearer ', ''), secretKey);
-         return { user };
-      } catch (error) {
-         return 'there is error token', error;
-      }
-   },
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        try {
+            const token = req.headers.authorization || '';
+            const user = jwt.verify(token.replace('Bearer ', ''), secretKey);
+            return { user };
+        } catch (error) {
+            return 'there is error token', error;
+        }
+    },
 });
 
 server.applyMiddleware({ app });
 const port = 3000;
 app.listen(port, () => {
-   console.log(`http://localhost:${port}/graphql`);
+    console.log(`http://localhost:${port}/graphql`);
 });
